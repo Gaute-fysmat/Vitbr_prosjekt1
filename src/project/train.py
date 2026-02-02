3934e6afa1824c044fa032f95562ec3166cdb02e
BrekkeSoegaard <morten.b.sogard@gmail.com>
2026-02-02 23:12:29 +0100

    Update train.py

--- src/project/train.py
+++ src/project/train.py
@@ -41,6 +41,8 @@ def train_nn(
         #  cfg.lambda_data, cfg.lambda_ic
         val , grads = jax.value_and_grad(lambda nn_params: (cfg.lambda_data * data_loss(nn_params, sensor_data, cfg)
                         + cfg.lambda_ic * ic_loss(nn_params, ic_epoch, cfg)), has_aux=True)(nn_params)
+        
+        
         nn_params, adam_state = adam_step(nn_params, grads, adam_state, lr=cfg.learning_rate)
 
     #######################################################################
